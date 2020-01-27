9815f8ffd5c80461d2a1faf41699c5f51ad74ac0
MaryannKamau <maryannkamauboo@gmail.com>
2020-01-27 20:25:35 +0300

    first files

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ cors = require('cors');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.get('/',(req,res)=>{
+    res.send(okay)
+})
 app.post('/client', client)
 var server = app.listen(5000, function () {
     console.log('Server is running..');
